b1656a1744ed9df1a831ee8104beeb283bebd893
Joran Dob <joran998@gmail.com>
2017-05-06 14:34:01 +0200

    Aan details pagina req element toegevoegd

--- libs/details.php
+++ libs/details.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-type:application/json;charset=utf-8');
 $d_url = $_GET["url"];
+$req = $_GET["req"];
 
 $curl3 = curl_init();
 curl_setopt($curl3, CURLOPT_SSL_VERIFYPEER, false);
@@ -19,31 +20,41 @@ $html_details_d->load($d_c_d);
 
 
 echo "[";
-foreach ($html_details_d->find('div[class=grid-5 alpha]') as $info_div1) {
-  foreach ($info_div1->find('li') as $li) {
+if ($req == "overig") {
+    $p_overig = $html_details_d->find('p', 0);
     echo '{';
-    echo '"opties":"' . $li->plaintext . '"';
-    echo '},';
-  }
-}
-foreach ($html_details_d->find('div[class=grid-5 alpha omega]') as $info_div2) {
-  $li_n = 0;
-
-
-  $last = count($info_div2->find('li'));
-  foreach ($info_div2->find('li') as $li) {
-    $li_n = $li_n+1;
-
-    echo '{';
-    echo '"opties":"' . $li->plaintext . '"';
-    if ($li_n==$last) {
+    echo '"overig":"' . $p_overig->plaintext . '"';
+    echo '}';
+} elseif ($req == "description") {
+    foreach ($html_details_d->find('p[itemprop=description]') as $p_des) {
+        echo '{';
+        echo '"description":"' . preg_replace( "/\r|\n/", "", $p_des->plaintext ) . '"';
         echo '}';
-    } else {
-        echo '},';
     }
-  }
+} elseif ($req == "opties") {
+    foreach ($html_details_d->find('div[class=grid-5 alpha]') as $info_div1) {
+        foreach ($info_div1->find('li') as $li) {
+            echo '{';
+            echo '"opties":"' . $li->plaintext . '"';
+            echo '},';
+        }
+    }
+    foreach ($html_details_d->find('div[class=grid-5 alpha omega]') as $info_div2) {
+        $li_n = 0;
+
+
+        $last = count($info_div2->find('li'));
+        foreach ($info_div2->find('li') as $li) {
+            $li_n = $li_n+1;
+
+            echo '{';
+            echo '"opties":"' . $li->plaintext . '"';
+            if ($li_n==$last) {
+                echo '}';
+            } else {
+                echo '},';
+            }
+        }
+    }
 }
-
-
   echo "]";
-?>
